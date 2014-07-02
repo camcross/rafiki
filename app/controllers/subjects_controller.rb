161e43f494d17ecd8740194b0829bd86d08db0bb
Cameron Cross <camcross94@gmail.com>
2014-07-02 22:03:26 +0200

    Created first view-- Subjects view is rendered, routed and displays info

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,19 @@
 class SubjectsController < ApplicationController
+	def index
+		@subjects = Subject.all
+	end	
+
 	def new
 		@subject = Subject.new
 	end
+
+	def create
+		@subject = Subject.new(subject_params)
+		@subject.save
+		redirect_to :action => :index
+	end
+	
+	def subject_params
+		params.require(:subject).permit(:name)
+	end	
 end
