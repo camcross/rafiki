7d9b93d60fb646433401c88a028f09a5e5ffdf65
Cameron Cross <camcross94@gmail.com>
2014-07-02 22:31:00 +0200

    Added two views, subjects#index and courses#index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,19 @@
 class CoursesController < ApplicationController
+	def index
+		@subjects = Subject.all
+	end	
+
 	def new
-		@course = Course.new
+		@subject = Subject.new
 	end
+
+	def create
+		@subject = Subject.new(subject_params)
+		@subject.save
+		redirect_to :action => :index
+	end
+	
+	def subject_params
+		params.require(:subject).permit(:name)
+	end	
 end
