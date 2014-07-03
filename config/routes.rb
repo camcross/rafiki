6b9db77475f2bd920f49667abf73dad1d79486b0
Cameron Cross <camcross94@gmail.com>
2014-07-03 23:15:52 +0200

    Fixed bugs dealing with sign in; this commit contains ALL CORRECT SIGN IN/UP/OUT INFO

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
   resources :courses, :only => [:index, :new, :create,]
 
-  resources :users, :only => [:index, :new, :create]
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
