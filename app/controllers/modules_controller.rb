d1a6c1f98e587fb84c5af2da6452df2e382cf3a9
Cameron Cross <camcross94@gmail.com>
2014-07-10 01:22:34 +0200

    Deleted Stuff

--- app/controllers/modules_controller.rb
+++
@@ -1,7 +0,0 @@
-class ModulesController < ApplicationController
-	 #forces the user to log in before doing anything at all
-  	before_action :authenticate_user!
-	def new
-		@module = Module.new
-	end
-end
