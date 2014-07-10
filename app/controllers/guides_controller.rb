27a6a4caaceb604c575f97eb8b4b8ed88a7e52ad
Cameron Cross <camcross94@gmail.com>
2014-07-10 11:22:18 +0200

    Changed a faulty action in the guides controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,6 @@
 class GuidesController < ApplicationController
 	 #forces the user to log in before doing anything at all
-  	wbefore_action :authenticate_user!
+  	before_action :authenticate_user!
 	def new
 		@guide = Guide.new
 	end
